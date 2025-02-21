256b697fe5eda0a0ee87660749f3598f4ab5ba24
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-21 16:31:47 +0100

    add second snack

--- main.js
+++ main.js
@@ -46,5 +46,93 @@ function getHeaviestBike() {
     return heaviestBike
 }
 
-
+console.log("snack 1---------------------------------------------------------------------------------------------------------------");
 console.log(getHeaviestBike());
+alert(getHeaviestBike())
+console.log("snack 2---------------------------------------------------------------------------------------------------------------");
+
+
+//snack 2
+
+/*Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+const soccerTeams = [
+    {
+        nome: "squadra1",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra2",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra3",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra4",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra5",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra6",
+        punti: 0,
+        'falli subiti': 0,
+    },
+    {
+        nome: "squadra7",
+        punti: 0,
+        'falli subiti': 0,
+    },
+
+]
+
+assignRandomPoints()
+assignRandomPenalties()
+
+console.log(`this is the full soccerteam parameters`);
+console.log(soccerTeams);
+
+console.log(`this is the shortened soccerteam parameters`);
+console.log(shortenSoccerTeamParameters());
+
+
+
+
+function assignRandomPoints() {
+    for (let i = 0; i < soccerTeams.length; i++) {
+        soccerTeams[i].punti = Math.floor(Math.random() * 30)
+    }
+}
+
+function assignRandomPenalties() {
+    for (let i = 0; i < soccerTeams.length; i++) {
+        soccerTeams[i]["falli subiti"] = Math.floor(Math.random() * 10)
+    }
+}
+
+function shortenSoccerTeamParameters() {
+    const shorterSoccerTeams = []
+    for (let i = 0; i < soccerTeams.length; i++) {
+        const { punti, ...rest } = soccerTeams[i];
+        shorterSoccerTeams.push(rest)
+    }
+
+    return shorterSoccerTeams
+}
+
+
+
+
